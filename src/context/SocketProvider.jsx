c8c6131f0d6576d001fc2be8171f714f9e7976e5
Rishabh Gupta <rishabhgupta@Rishabhs-MacBoo.local>
2026-04-03 21:50:52 +0530

    update backend url

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -8,7 +8,7 @@ export const useSocket = () => {
 }
 
 const SocketProvider = (props) => {
-     const socket = useMemo(()=> io('http://localhost:8000'),[]);
+     const socket = useMemo(()=> io('https://vediochatappbackend.onrender.com'),[]);
   return (
      <SocketContext.Provider value={socket}>
            {props.children }
